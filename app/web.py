7cc1efc5728290f6a615ee93d8e84b6081deeae5
Tyler Copple <j.tylercopple@gmail.com>
2020-03-17 00:35:35 -0400

    Updates to web.py and requirements.txt as the initial sheel of the application.

--- app/web.py
+++ app/web.py
@@ -5,38 +5,25 @@ import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
 from config import Config
-from model_parameters import ModelParameters
+
+from layout import Layout
+from parameters import Parameters
+from vizualizations import Vizualizations
 
 
-app = dash.Dash(__name__, external_stylesheets=Config.STYLESHEET)
+app = dash.Dash(__name__)
 
+app.layout = Layout().render(
+    parameter_renderer=Parameters().render, output_renderer=Vizualizations().render
+)
 
+#  @app.callback(Output("tabs-content", "children"), [Input("tabs", "value")])
+#  def render_content(tab):
+#  if tab == "tab-1":
+#  return html.Div([html.H3("Tab content 1")])
+#  elif tab == "tab-2":
+#  return html.Div([html.H3("Tab content 2")])
 
-app.layout = html.Div([
-    html.Span(
-        [html.H1(children="Parameters")],
-        id='parameters'
-        ),
-    html.Span([
-        dcc.Tabs(id="tabs", value='tab-1', children=[
-            dcc.Tab(label='Tab one', value='tab-1'),
-            dcc.Tab(label='Tab two', value='tab-2'),
-            ]),
-        html.Div(id='tabs-content')]
-    )
-])
-
-@app.callback(Output('tabs-content', 'children'),
-        [Input('tabs', 'value')])
-def render_content(tab):
-    if tab == 'tab-1':
-        return html.Div([
-            html.H3('Tab content 1')
-            ])
-    elif tab == 'tab-2':
-        return html.Div([
-            html.H3('Tab content 2')
-            ])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
