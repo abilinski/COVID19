899ffec2f37c79b4a9bbea8a3f9efd631fa2eec6
Tyler Copple <j.tylercopple@gmail.com>
2020-03-17 22:40:02 -0400

    Connecting submit button to updates of parameter values in right hand pane.

--- web.py
+++ web.py
@@ -19,7 +19,7 @@ vizualizations = Vizualizations()
 
 def make_submit_control():
     return html.Div(
-        [html.Button("Submit", id="submit-button")],
+        [html.Button("Submit", id="submit-button", n_clicks=0)],
         #  title="Click to send all of the control values to the spectrometer.",
         #  className="control",
     )
@@ -32,15 +32,31 @@ app.layout = Layout().render(
 
 @app.callback(
     Output("submit-status", "children"),
-    [Input("submit-button", "n_clicks")],
+    [Input("submit-button", "n_clicks_timestamp")],
     state=[State(ctrl.selector, ctrl.get_value()) for ctrl in parameters.parameter_controls]
 )
-def calculate(n_clicks, *state):
-    if n_clicks is None:
+def calculate(n_clicks_timestamp, *state):
+    if n_clicks_timestamp is None:
         raise PreventUpdate
 
-    print(state)
-    return "Foo Bar Baz"
+    controls = parameters.parameter_controls
+    # dictionary of commands; component id and associated value
+    commands = {controls[i].selector: state[i] for i in range(len(controls))}
+
+    #  failed, succeeded = spec.send_control_values(commands)
+#
+    summary = []
+
+    summary.append("The following parameters were successfully updated: ")
+    summary.append(html.Br())
+    summary.append(html.Br())
+
+    for key, value in commands.items():
+        summary.append(f"{key.upper()}: {value}")
+        summary.append(html.Br())
+
+
+    return html.Div(summary)
 
 
 if __name__ == "__main__":
