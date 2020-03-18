784505869f55bdb8fe7cdf6be029576ad44c3249
Luke Massa <lmassa@tripadvisor.com>
2020-03-18 11:58:28 -0400

    Make path absolute

--- webapp/model.py
+++ webapp/model.py
@@ -4,11 +4,17 @@ Actually run the model
 """
 
 import subprocess
+import os
+
+R_SCRIPT="helloworld.R"
 
 
 def run(parameters):
 
-    cmd = ["Rscript", "helloworld.R"]
+    abspath = os.path.abspath(R_SCRIPT)
+
+
+    cmd = ["Rscript", abspath]
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = proc.communicate()
     if proc.returncode != 0:
