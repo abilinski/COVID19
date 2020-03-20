ca2f2a292b4f161dff322a7877974eb5dddb42c6
Tyler Copple <j.tylercopple@gmail.com>
2020-03-19 23:57:11 -0400

    Migration to SemanticUI as front end css framework.

--- webapp/vizualizations.py
+++ webapp/vizualizations.py
@@ -5,12 +5,23 @@ from dash.exceptions import PreventUpdate
 
 from dash.dependencies import Input, Output, State
 
+
 class Vizualizations:
     def __init__(self, charts):
         self.charts = charts
 
     def render(self):
-        tabs = [dcc.Tab(label=f"Tab {i}", children=[chart]) for i, chart in  enumerate(self.charts)]
-        return html.Div([
-            dcc.Tabs(id="tabs", value='tab-1', children=tabs)
-        ])
+        html_menu = html.Div(
+            className="ui tabular menu",
+            children=[
+                html.A(className="item", children=i, **{"data-tab": i})
+                for i, chart in enumerate(self.charts)
+            ],
+        )
+
+        html_tabs = [
+            html.Div(className="ui tab", children=chart, **{"data-tab": i})
+            for i, chart in enumerate(self.charts)
+        ]
+
+        return html.Div(className="ui", children=[html_menu] + html_tabs)
