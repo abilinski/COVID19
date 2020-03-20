c742b303ea877d8f5189221d4190961e6b401870
Tyler Copple <j.tylercopple@gmail.com>
2020-03-19 23:57:11 -0400

    Migration to SemanticUI as front end css framework.

--- webapp/parameters.py
+++ webapp/parameters.py
@@ -5,114 +5,123 @@ import dash_core_components as dcc
 import dash_daq as daq
 
 
-def __percent_selector_params():
-    def percentify(value):
-        return "{:.0%}".format(value)
+class Control:
+    def __init__(self, label, selector, builder, classes=None, attrs=None):
+        self.label = label
+        self.selector = selector
+        self.builder = builder
+        self.classes = classes
+        self.attrs = attrs
 
-    step_size = 0.05
-    steps = [i * step_size for i in range(0, int(1 / step_size), 1)]
+    def render(self):
+        component = self.builder(id=self.selector, **self.attrs)
 
-    mark_size = 0.10
-    marks = [0] + [i * mark_size for i in range(0, int(1 / mark_size), 1)] + [1]
-    marks_dict = {mark: percentify(mark) for mark in marks}
+        html_label = html.P(self.label)
+        html_component = html.Div(className=self.classes, children=component)
 
-    return {"min": 0, "max": 1, "step": step_size, "value": 0.10, "marks": marks_dict}
+        return html.Div(
+            className="ui secondary segment", children=[html_label, html_component,],
+        )
+
+    def value_key(self):
+        return "value"
+
+    def update_value(self, new_value):
+        raise Exception("Not yet implemented.")
+        #  self.component_attr[self.value_key()] = new_value
 
 
-def number_of_cases_input():
-    return (
+class SliderControl(Control):
+    def __init__(self, label, selector, attrs):
+        super().__init__(label, selector, dcc.Slider, attrs=attrs)
+
+
+class PercentSliderControl(SliderControl):
+    def __init__(self, label, selector, default_value=0.10):
+        super().__init__(
+            label, selector, attrs=PercentSliderControl.__generate_attrs(default_value)
+        )
+
+    @classmethod
+    def __generate_attrs(cls, default_value=0.10):
+        def percentify(value):
+            return "{:.0%}".format(value)
+
+        step_size = 0.05
+        steps = [i * step_size for i in range(0, int(1 / step_size), 1)]
+
+        mark_size = 0.10
+        marks = [0] + [i * mark_size for i in range(0, int(1 / mark_size), 1)] + [1]
+        marks_dict = {mark: percentify(mark) for mark in marks}
+
+        return {
+            "min": 0,
+            "max": 1,
+            "step": step_size,
+            "value": default_value,
+            "marks": marks_dict,
+        }
+
+
+class InputControl(Control):
+    def __init__(self, label, selector, attrs=None):
+        super().__init__(
+            label, selector, dcc.Input, classes="ui fluid input", attrs=attrs
+        )
+
+
+class ToggleControl(Control):
+    def __init__(self, label, selector, attrs):
+        super().__init__(
+            label, selector, dcc.RadioItems, classes="ui radio checkbox", attrs=attrs
+        )
+
+    def value_key(self):
+        return "value"
+
+
+CONTROLS = [
+    InputControl(
         "Number of Cases",
         "parameter-cases-n",
-        dcc.Input,
-        dict(placeholder=0, type="number", min=0, value=0),
-    )
-
-
-def proportion_of_cases_slider():
-    return (
-        "Proportion of Cases Reported",
-        "parameter-cases-reported",
-        dcc.Slider,
-        __percent_selector_params(),
-    )
-
-
-def school_closures_toggle():
-    return (
+        attrs=dict(placeholder=0, type="number", min=0, value=0),
+    ),
+    InputControl(
+        "Number of ICU Beds",
+        "parameter-icu-beds-n",
+        attrs=dict(placeholder=0, type="number", min=0, value=0),
+    ),
+    InputControl(
+        "Number of Ventilators",
+        "parameter-ventilators-n",
+        attrs=dict(placeholder=0, type="number", min=0, value=0),
+    ),
+    PercentSliderControl("Proportion of Cases Reported", "parameter-cases-reported"),
+    ToggleControl(
         "School Closures",
         "parameter-schools-closed",
-        #  daq.BooleanSwitch(on=False,),
-        dcc.RadioItems,
         dict(
             options=[{"label": "On", "value": 1}, {"label": "Off", "value": 0},],
             value=0,
         ),
-    )
-
-
-def no_contact_zone(age_group):
-    return (
-        f"Proportion of {age_group} in no contact zone.",
-        f"parameter-no-contact-{age_group}",
-        dcc.Slider,
-        __percent_selector_params(),
-    )
-
-
-def render_control(control):
-    try:
-        ctor = control.klass
-    except AttributeError:
-        ctor = control.klass
-
-    component = ctor(id=control.selector, **control.attrs)
-
-    control = html.Div(
-        id=f"{control.selector}-wrapper",
-        children=[html.P(control.label), component],
-    )
-
-    return control
-
-
-class ParameterControl:
-    def __init__(self, label, selector, klass, attrs):
-        self.label = label
-        self.selector = selector
-        self.klass = klass
-        self.attrs = attrs
-
-    def get_value(self):
-        if "value" in self.attrs:
-            return "value"
-        elif "on" in self.attrs:
-            return "on"
-
-    # changes value ('on' or 'value', etc.)
-    def update_value(self, new_value):
-        self.component_attr[self.val_string()] = new_value
+    ),
+    PercentSliderControl(
+        f"Proportion of 0-20 in no contact zone.", f"parameter-no-contact-0-20",
+    ),
+    PercentSliderControl(
+        f"Proportion of 21-59 in no contact zone.", f"parameter-no-contact-21-59",
+    ),
+    PercentSliderControl(
+        f"Proportion of 60+ in no contact zone.", f"parameter-no-contact-60+",
+    ),
+]
 
 
 class Parameters:
-    PARAMETER_FUNCS = [
-        number_of_cases_input,
-        proportion_of_cases_slider,
-        school_closures_toggle,
-        functools.partial(no_contact_zone, "0-20"),
-        functools.partial(no_contact_zone, "21-64"),
-        functools.partial(no_contact_zone, "65+"),
-    ]
-
-    def __init__(self, controls=PARAMETER_FUNCS):
-        self.parameter_controls = []
-
-        for f in Parameters.PARAMETER_FUNCS:
-            attrs = f()
-            ctrl = ParameterControl(*attrs)
-            self.parameter_controls.append(ctrl)
+    def __init__(self, controls=CONTROLS):
+        self.parameter_controls = controls
 
     def render(self):
         return html.Div(
-            id="parameters",
-            children=[render_control(c) for c in self.parameter_controls],
+            id="parameters", children=[c.render() for c in self.parameter_controls],
         )
