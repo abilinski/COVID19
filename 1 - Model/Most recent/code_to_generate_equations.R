3c2ef8e9b43ce08d5d94e205f79740efc15f4fc0
zhulin0406 <32205290+zhulin0406@users.noreply.github.com>
2020-03-19 17:32:49 -0700

    1. code to generate equations updated to have detected and undetected compartments for both I (symptomatic) and A (asymptomatic); 2. Updated equations in the main code in a new Rscript; 3. Simple calibration of p to SCC time series data (not using but keeping it here in case) 4. Added the latest params file received.

--- 1 - Model/Most recent/code_to_generate_equations.R
+++ 1 - Model/Most recent/code_to_generate_equations.R
@@ -1,22 +1,53 @@
 make_eqn_strat = function(group){
-  S = paste("dS", group, "dt = -S", group,
-            "*k_susp*p*(k_inf*v1", group, "*I1/N1 + vA1", group, "*A1/N1 + v2", group,
-            "*I2/N2 + vA2", group, "*A2/N2 + v3", group, "*I3/N3 + vA3", group, "*A3/N3 + k_inf*v1Q", group,
-            "*I1Q/N1Q + vA1Q", group, "*A1Q/N1Q + v2Q", group, "*I2Q/N2Q + vA2Q", group, "*A2Q/N2Q + v3Q", group,
-            "*I3Q/N3Q + vA3Q", group, "*A3Q/N3Q)",
-            sep = "")
-  E = paste("dE", group, "dt = -delta*E", group, "+ S", group,
-            "*k_susp*p*(k_inf*v1", group, "*I1/N1 + vA1", group, "*A1/N1 + v2", group,
-            "*I2/N2 + vA2", group, "*A2/N2 + v3", group, "*I3/N3 + vA3", group, "*A3/N3 + k_inf*v1Q", group,
-            "*I1Q/N1Q + vA1Q", group, "*A1Q/N1Q + v2Q", group, "*I2Q/N2Q + vA2Q", group, "*A2Q/N2Q + v3Q", group,
-            "*I3Q/N3Q + vA3Q", group, "*A3Q/N3Q)",
-            sep = "")
-  A = paste("dA", group, "dt = alpha", group, "*delta*E", group, " - gamma*A", group, sep = "")
-  I = paste("dI", group, "dt = (1-alpha", group, ")*delta*E", group, "- (m", group, "*omega + (1-m", group, ")*gamma)*I",group, sep = "")
-  R = paste("dR", group, "dt = gamma*A", group, "+ (1-m", group, ")*gamma*I", group, sep = "")
-  return(c(S, E, I, A, R))
+  S = paste("dS", group, "dt = -S", group, "*k_susp*p*(k_inf*v1", group, "*(UI1+DI1)/N1 + vA1", group, "*(UA1+DA1)/N1 + v2", group, "*(UI2+DI2)/N2 + vA2", group, "*(UA2+DA2)/N2 + v3", group, "*(UI3+DI3)/N3 + vA3", group, "*(UA3+DA3)/N3 + k_inf*v1Q", group, "*(UI1Q+DI1Q)/N1Q + vA1Q", group, "*(UA1Q+DA1Q)/N1Q + v2Q", group, "*(UI2Q+DI2Q)/N2Q + vA2Q", group, "*(UA2Q+DA2Q)/N2Q + v3Q", group, "*(UI3Q+DI3Q)/N3Q + vA3Q", group, "*(UA3Q+DA3Q)/N3Q)", sep = "")
+  
+  E = paste("dE", group, "dt = -delta*E", group, " +S", group, "*k_susp*p*(k_inf*v1", group, "*(UI1+DI1)/N1 + vA1", group, "*(UA1+DA1)/N1 + v2", group, "*(UI2+DI2)/N2 + vA2", group, "*(UA2+DA2)/N2 + v3", group, "*(UI3+DI3)/N3 + vA3", group, "*(UA3+DA3)/N3 + k_inf*v1Q", group, "*(UI1Q+DI1Q)/N1Q + vA1Q", group, "*(UA1Q+DA1Q)/N1Q + v2Q", group, "*(UI2Q+DI2Q)/N2Q + vA2Q", group, "*(UA2Q+DA2Q)/N2Q + v3Q", group, "*(UI3Q+DI3Q)/N3Q + vA3Q", group, "*(UA3Q+DA3Q)/N3Q)", sep = "")
+  
+  UI = paste("dUI", group, "dt = (1-alpha", group, ")*delta*E", group, " - (rdetecti + m", group, "*omega + (1-m", group, ")*gamma)*UI", group, sep = "")
+  
+  DI = paste("dDI", group, "dt = rdetecti*UI", group, " - (m", group, "*omega + (1-m", group, ")*gamma)*DI", group, sep = "")
+  
+  UA = paste("dUA", group, "dt = alpha", group, "*delta*E", group, " - (rdetecta + gamma)*UA", group, sep = "")
+  
+  DA = paste("dDA", group, "dt = rdetecta*UA", group, " - gamma*DA", group, sep = "")
+  
+  R = paste("dR", group, "dt = (1-m", group, ")*gamma*(UI", group,"+DI", group, ")"," + gamma*(UA", group, "+DA", group, ")", sep = "")
+  
+  IT = paste("It", group, " = (1-alpha", group, ")*delta*E", group, sep = "")
+  
+  AT = paste("At", group, " = alpha", group, "*delta*E", group, sep = "")
+  
+  DT = paste("Dt", group, " = m", group, "*omega*(UI", group, "+DI", group, ")", sep = "")
+  
+  return(c(S, E, UI, DI, UA, DA, R, IT, AT, DT))
 }
 
+make_eqn_strat(2)
+#####note: when generating for 2 and 3, delete *k_susp in both S and E
 
-make_eqn_strat(1)
+make_eqn_strat_Q = function(group){
+  S = paste("dS", group, "Qdt = -S", group, "Q*k_susp*p*(k_inf*v1", group, "Q*(UI1+DI1)/N1 + vA1", group, "Q*(UA1+DA1)/N1 + v2", group, "Q*(UI2+DI2)/N2 + vA2", group, "Q*(UA2+DA2)/N2 + v3", group, "Q*(UI3+DI3)/N3 + vA3", group, "Q*(UA3+DA3)/N3 + k_inf*v1Q", group, "Q*(UI1Q+DI1Q)/N1Q + vA1Q", group, "Q*(UA1Q+DA1Q)/N1Q + v2Q", group, "Q*(UI2Q+DI2Q)/N2Q + vA2Q", group, "Q*(UA2Q+DA2Q)/N2Q + v3Q", group, "Q*(UI3Q+DI3Q)/N3Q + vA3Q", group, "Q*(UA3Q+DA3Q)/N3Q)", sep = "")
+  
+  E = paste("dE", group, "Qdt = -delta*E", group, "Q + S", group, "Q*k_susp*p*(k_inf*v1", group, "Q*(UI1+DI1)/N1 + vA1", group, "Q*(UA1+DA1)/N1 + v2", group, "Q*(UI2+DI2)/N2 + vA2", group, "Q*(UA2+DA2)/N2 + v3", group, "Q*(UI3+DI3)/N3 + vA3", group, "Q*(UA3+DA3)/N3 + k_inf*v1Q", group, "Q*(UI1Q+DI1Q)/N1Q + vA1Q", group, "Q*(UA1Q+DA1Q)/N1Q + v2Q", group, "Q*(UI2Q+DI2Q)/N2Q + vA2Q", group, "Q*(UA2Q+DA2Q)/N2Q + v3Q", group, "Q*(UI3Q+DI3Q)/N3Q + vA3Q", group, "Q*(UA3Q+DA3Q)/N3Q)", sep = "")
+  
+  UI = paste("dUI", group, "Qdt = (1-alpha", group, "Q)*delta*E", group, "Q - (rdetecti + m", group, "Q*omega + (1-m", group, "Q)*gamma)*UI", group, "Q",sep = "")
+  
+  DI = paste("dDI", group, "Qdt = rdetecti*UI", group, "Q - (m", group, "Q*omega + (1-m", group, "Q)*gamma)*DI", group, "Q", sep = "")
+  
+  UA = paste("dUA", group, "Qdt = alpha", group, "Q*delta*E", group, "Q - (rdetecta + gamma)*UA", group, "Q", sep = "")
+  
+  DA = paste("dDA", group, "Qdt = rdetecta*UA", group, "Q - gamma*DA", group, "Q", sep = "")
+  
+  R = paste("dR", group, "Qdt = (1-m", group, "Q)*gamma*(UI", group, "Q+DI", group, "Q)", " + gamma*(UA", group, "Q+DA", group, "Q)", sep = "")
+  
+  IT = paste("It", group, "Q = (1-alpha", group, "Q)*delta*E", group, "Q", sep = "")
+  
+  AT = paste("At", group, "Q = alpha", group, "Q*delta*E", group, "Q", sep = "")
+  
+  DT = paste("Dt", group, "Q = m", group, "Q*omega*(UI", group, "Q+DI", group, "Q)", sep = "")
+  
+  return(c(S, E, UI, DI, UA, DA, R, IT, AT, DT))
+}
 
+make_eqn_strat_Q(3)
+#####note: when generating for 2 and 3, delete *k_susp in both S and E
