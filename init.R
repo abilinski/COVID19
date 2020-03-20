0eee21b654453c41aea339fda90641de59c6bb6f
Luke Massa <lmassa@tripadvisor.com>
2020-03-20 15:35:15 -0400

    Cleaning up aptfile

--- init.R
+++ init.R
@@ -1,7 +1,7 @@
 ### Blank for now
 
 install.packages('tidyverse', repo="http://cran.rstudio.com/")
-install.packages('matlib', repo="http://cran.rstudio.com/")
+#install.packages('matlib', repo="http://cran.rstudio.com/")
 #library(deSolve)
 #library(ggthemes)
 #library(tictoc)
