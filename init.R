504e5504867d4619f1e67edd03181a8f461a4b70
Luke Massa <lmassa@tripadvisor.com>
2020-03-20 00:22:17 -0400

    Added init

--- init.R
+++ init.R
@@ -1,6 +1,7 @@
 ### Blank for now
 
 install.packages('tidyverse', repo="http://cran.rstudio.com/")
+install.packages('matlab', repo="http://cran.rstudio.com/")
 #library(deSolve)
 #library(ggthemes)
 #library(tictoc)
