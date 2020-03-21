412d9ac065e218c7420d852ea29bc660c7320237
chekao <kaoyh@umich.edu>
2020-03-20 22:27:09 -0400

    fix bugs after "merge intervention_plot" branch

--- 5 - Shiny Apps/covid_shiny_app_v0.1/app.R
+++ 5 - Shiny Apps/covid_shiny_app_v0.1/app.R
@@ -33,10 +33,10 @@ ui <- fluidPage(
         #tabPanel("Intervention", plotOutput("intervention"))
         
     ),
+    actionButton("reset_inputs", "Reset All Parameters"),
     #verbatimTextOutput("renderprint"),
     
     hr(),
-<<<<<<< HEAD
     column(6,
            h3("Base case parameters"),
            wellPanel(
@@ -51,9 +51,9 @@ ui <- fluidPage(
                    #maybe put matrix here
             ),
             column(4,
-                   sliderInput("s", label = "s: Frc socially distanced)", min = 0, 
+                   sliderInput("s", label = "s: Frc socially distanced", min = 0, 
                                max = 1, value = 0),
-                   sliderInput("e", label = "e: Social distance multiplier)", min = 0, 
+                   sliderInput("e", label = "e: Social distance multiplier", min = 0, 
                                max = 1, value = 0),
                    sliderInput("p", label = "p: Pr(transmission/contact)", min = 0.01, 
                                max = 0.1, value = 0.05),
@@ -67,7 +67,6 @@ ui <- fluidPage(
                                max = 1, value = 0.1),
                    sliderInput("k_report", label = "k_report: rel rep rate for yng", min = 0, 
                                max = 1, value = 1)
-                   actionButton("reset_inputs", "Reset All Parameters")
             ),
             column(4,
                    sliderInput("alpha1", label = HTML("&alpha;1: Pr(asymp) yng"), min = 0, 
@@ -90,73 +89,12 @@ ui <- fluidPage(
                                max = 1, value = 1)
                    #p("*young+medium+old <= 1"),
                 ),
-            downloadButton("download", "Download parameters"),
+            downloadButton("download", "Download parameters")
                   
             )
-=======
-    
-    fluidRow(#class = "text-center",
-        column(2,
-               radioButtons("intervention", label = "Intervention",
-                            choices = list("yes" = 1, "no" = 2), selected = 2),
-               conditionalPanel(
-                   condition = "input.intervention == 1",
-                   numericInput("days_out1", label="intervention starts at", value=15)
-               ),
-               numericInput("days_out2", label="simulation time (days)", value=30),
-               numericInput("R0", label="R0", value=2.2),
-               numericInput("delta", label=HTML("&delta;: 1/(dur of incub)"), value=0.2),
-               numericInput("gamma", label=HTML("&gamma;: 1/(dur of infectious)"), value=0.2),
-               numericInput("n", label="n: total population", value=1938000)
-        ),
-        column(2,
-               sliderInput("s", label = "s: Frc socially distanced)", min = 0, 
-                           max = 1, value = 0),
-               sliderInput("e", label = "e: Social distance multiplier)", min = 0, 
-                           max = 1, value = 0),
-               sliderInput("p", label = "p: Pr(transmission/contact)", min = 0.01, 
-                           max = 0.1, value = 0.05),
-               sliderInput("kappa", label = HTML("&kappa;: rel. Pr(trans) for asymp"), min = 0, 
-                           max = 1, value = 0.375),
-               actionButton("reset_inputs", "Reset All Parameters")
-        ),
-        column(2,
-               sliderInput("alpha1", label = HTML("&alpha;1: Pr(asymp) yng"), min = 0, 
-                           max = 1, value = 0.75),
-               sliderInput("alpha2", label = HTML("&alpha;2: Pr(asymp) med"), min = 0, 
-                           max = 1, value = 0.3),
-               sliderInput("alpha3", label = HTML("&alpha;3: Pr(asymp) old"), min = 0, 
-                           max = 1, value = 0.3),
-               sliderInput("c", label = "c: Reporting rate", min = 0, 
-                           max = 1, value = 0.13)
-        ),
-        column(2,
-               sliderInput("m1", label = "m1: mortality yng", min = 0, 
-                           max = 1, value = 0),
-               sliderInput("m2", label = "m2: mortality med", min = 0, 
-                           max = 1, value = 0.005),
-               sliderInput("m3", label = "m3: mortality old", min = 0, 
-                           max = 1, value = 0.1)
-        ),
-        column(2,
-               sliderInput("young", label = "Frc young pop", min = 0, 
-                           max = 1, value = 0.24),
-               sliderInput("medium", label = "Frc medium pop", min = 0, 
-                           max = 1, value = 0.6),
-               sliderInput("old", label = "Frc old pop", min = 0, 
-                           max = 1, value = 0.15),
-               #p("*young+medium+old <= 1"),
-        ),
-        column(2,
-               sliderInput("k_report", label = "k_report: rel rep rate for yng", min = 0, 
-                           max = 1, value = 1),
-               sliderInput("k_inf", label = "k_inf: rel infectiousness for yng", min = 0, 
-                           max = 1, value = 1),
-               sliderInput("k_susp", label = "k_susp: rel. suscep for yng", min = 0, 
-                           max = 1, value = 1)
->>>>>>> e793f348d09f62912a260718a9321a82c3de941a
-        )
+           )
     ),
+    
     column(6,
            h3("Intervention parameters"),
            wellPanel(
@@ -165,47 +103,47 @@ ui <- fluidPage(
                           #umericInput("sim_time", label="simulation time (days)", value=30),
                           numericInput("int_time", label="intervention starts at", value=15),
                           numericInput("R0_int", label="R0", value=2.2),
-                          numericInput("delta_int", label=HTML("&delta;:"), value=0.2),
-                          numericInput("gamma_int", label=HTML("&gamma;:"), value=0.2),
-                          numericInput("n_int", label="n", value=1938000)
+                          numericInput("delta_int", label=HTML("&delta;: 1/(dur of incub)"), value=0.2),
+                          numericInput("gamma_int", label=HTML("&gamma;: 1/(dur of infectious)"), value=0.2),
+                          numericInput("n_int", label="n: total population", value=1938000)
                           #maybe put matrix here
                    ),
                    column(4,
-                          sliderInput("s_int", label = "s", min = 0, 
+                          sliderInput("s_int", label = "s: Frc socially distanced", min = 0, 
                                       max = 1, value = 0),
-                          sliderInput("e_int", label = "e", min = 0, 
+                          sliderInput("e_int", label = "e: Social distance multiplier", min = 0, 
                                       max = 1, value = 0),
-                          sliderInput("p_int", label = "p", min = 0.01, 
+                          sliderInput("p_int", label = "p: Pr(transmission/contact)", min = 0.01, 
                                       max = 0.1, value = 0.05),
-                          sliderInput("kappa_int", label = HTML("&kappa;:"), min = 0, 
+                          sliderInput("kappa_int", label = HTML("&kappa;: rel. Pr(trans) for asymp"), min = 0, 
                                       max = 1, value = 0.375),
-                          sliderInput("m1_int", label = "m1", min = 0, 
+                          sliderInput("m1_int", label = "m1: mortality yng", min = 0, 
                                       max = 1, value = 0),
-                          sliderInput("m2_int", label = "m2", min = 0, 
+                          sliderInput("m2_int", label = "m2: mortality med", min = 0, 
                                       max = 1, value = 0.005),
-                          sliderInput("m3_int", label = "m3", min = 0, 
+                          sliderInput("m3_int", label = "m3: mortality old", min = 0, 
                                       max = 1, value = 0.1),
-                          sliderInput("k_report_int", label = "k_report", min = 0, 
+                          sliderInput("k_report_int", label = "k_report: rel rep rate for yng", min = 0, 
                                       max = 1, value = 1)
                    ),
                    column(4,
-                          sliderInput("alpha1_int", label = HTML("&alpha;1"), min = 0, 
+                          sliderInput("alpha1_int", label = HTML("&alpha;1: Pr(asymp) yng"), min = 0, 
                                       max = 1, value = 0.75),
-                          sliderInput("alpha2_int", label = HTML("&alpha;2"), min = 0, 
+                          sliderInput("alpha2_int", label = HTML("&alpha;2: Pr(asymp) med"), min = 0, 
                                       max = 1, value = 0.3),
-                          sliderInput("alpha3_int", label = HTML("&alpha;1"), min = 0, 
+                          sliderInput("alpha3_int", label = HTML("&alpha;3: Pr(asymp) old"), min = 0, 
                                       max = 1, value = 0.3),
-                          sliderInput("c_int", label = "c", min = 0, 
+                          sliderInput("c_int", label = "c: Reporting rate", min = 0, 
                                       max = 1, value = 0.13),
-                          sliderInput("young_int", label = "youth_int", min = 0, 
+                          sliderInput("young_int", label = "Frc youth", min = 0, 
                                       max = 1, value = 0.24),
-                          sliderInput("medium_int", label = "adults_int", min = 0, 
+                          sliderInput("medium_int", label = "Frc adults", min = 0, 
                                       max = 1, value = 0.6),
-                          sliderInput("old_int", label = "older adults_int", min = 0, 
+                          sliderInput("old_int", label = "Frc older adults", min = 0, 
                                       max = 1, value = 0.15),
-                          sliderInput("k_inf_int", label = "k_inf", min = 0, 
+                          sliderInput("k_inf_int", label = "k_inf: rel infectiousness for yng", min = 0, 
                                       max = 1, value = 1),
-                          sliderInput("k_susp_int", label = "k_susp", min = 0, 
+                          sliderInput("k_susp_int", label = "k_susp: rel. suscep for yng", min = 0, 
                                       max = 1, value = 1)
                           #p("*young+medium+old <= 1"),
                    ),
@@ -215,6 +153,7 @@ ui <- fluidPage(
            )
     )
 )
+
 
 # Define server logic required to run the model and display the results
 server <- function(input, output) {
@@ -227,15 +166,14 @@ server <- function(input, output) {
         param_vec[param_names_base]<-as.numeric(temp[param_names_int])
         old_vec$Scenario<-'Base'
         param_vec$Scenario<-'Intervention'
-        test = run_param_vec(params = old_vec, params2 = NULL, days_out1 = input$sim_time,
-                             days_out2 = NULL, model_type = run_basic)
-        test_int = run_param_vec(params = old_vec, params2 = param_vec, days_out1 = input$int_time,
-                                 days_out2 = input$sim_time, model_type = run_int)
+        # test = run_param_vec(params = old_vec, params2 = NULL, days_out1 = input$sim_time,
+        #                      days_out2 = NULL, model_type = run_basic)
+        # test_int = run_param_vec(params = old_vec, params2 = param_vec, days_out1 = input$int_time,
+        #                          days_out2 = input$sim_time, model_type = run_int)
         print(old_vec)
         print(param_vec)
-        #print(as.numeric(temp[param_names_int]))
-        print(tail(test))
-        print(tail(test_int))
+        # print(tail(test))
+        # print(tail(test_int))
     })
     
     ## download adjusted base parameters
