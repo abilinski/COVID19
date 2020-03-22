901e018cd7940b0613e4bb068051c7bc2c077534
Luke Massa <lmassa@tripadvisor.com>
2020-03-22 10:59:03 -0400

    Run model on /model

--- webapp/model.py
+++ webapp/model.py
@@ -4,12 +4,12 @@ Actually run the model
 
 import subprocess
 import os
+import csv
 
 R_SCRIPT = "1 - Model/Most recent/run_model.R"
 
 
 def run(parameters):
-
     abspath = os.path.abspath(R_SCRIPT)
 
     cmd = ["Rscript", abspath]
@@ -17,5 +17,11 @@ def run(parameters):
     out, err = proc.communicate()
     if proc.returncode != 0:
         raise ValueError("Error running Rscript: %s %s" % (err, out))
-    print(type(out))
-    return out
+    out = out.decode('utf-8')
+    err = err.decode('utf-8')
+    csvfile = out.split('\n')
+    reader = csv.DictReader(csvfile)
+    result = list(reader)
+    # Result is an ordered dict; remove this line to keep it that way
+    result = [dict(x) for x in result]
+    return result
