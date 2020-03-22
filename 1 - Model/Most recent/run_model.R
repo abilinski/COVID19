a8c5cd372bd2106399557c17888b2da6f3b80112
Luke Massa <lmassa@tripadvisor.com>
2020-03-22 17:16:58 -0400

    Added code to use parameters

--- 1 - Model/Most recent/run_model.R
+++ 1 - Model/Most recent/run_model.R
@@ -1,10 +1,14 @@
+
+library("argparse")
+parser <- ArgumentParser()
+
+parser$add_argument("--parameters", default="./0 - Parameters/parameters_17_mar_2020.csv", help = "Parameter file")
+args <- parser$parse_args()
+
 dir.name = "1 - Model/Most recent/"
 source(file.path(dir.name, "model_3strat_18_mar_2020.R"))
 
-
-setwd("./0 - Parameters")
-# read in parameters
-df = read.csv("parameters_17_mar_2020.csv", as.is = T)
+df = read.csv(args$parameters, as.is = T)
 params = df[1,]
 # run scenario for 30 days
 test = run_param_vec(params = df[1,], params2 = NULL, days_out1 = 30,
