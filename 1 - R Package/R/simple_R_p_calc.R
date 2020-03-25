19f0d997d8fb9256d532eeda9f18b247f05a71c5
zhulin0406 <32205290+zhulin0406@users.noreply.github.com>
2020-03-25 11:16:10 -0700

    1. updated parameter file with detection parameters in R package inst folder; 2. put the new model.R including detection compartments in R package R folder (added in new code from the previous model.R that the detection code didn't have); 3. made simple R and p calculation from time series data a function and put into the R package R foler; 4. put the new equation generation code including detection compartments to R package R folder; 5. put the testing code into R package inst folder;

---
+++ 1 - R Package/R/simple_R_p_calc.R
@@ -0,0 +1,22 @@
+# fit exponential model with time series data to calculate R and transmission p
+exp_calc <- function(path, Tc=7.5, C=10, T=5) {
+  #path: the folder containing the time_series_SCC.csv file
+  #Tc: mean generation interval, mean duration between time of infection of a secondary infectee and the time of infection of its primary infector
+  #C: mean contact (calculated from contact matrix)
+  #T: infectiou period
+  #data from 2/25/20 to 3/15/20: it was announced on 2/27/20 that people who do not have travel/contact histry can be tested, so not taking data from too early
+  time_series <- read.csv(paste0(path,"time_series_SCC.csv"))
+  fit <- lm(log(time_series$cum_cases) ~ time_series$time)
+  # plot(time_series$time, time_series$cum_cases)
+  # lines(time_series$time, exp(fitted(fit)))
+  #coefficients of exponential model
+  a <- exp(fit$coefficients[[1]])
+  b <- exp(fit$coefficients[[2]])
+  # rate of exponential growth, per capita change in number of new cases per unit of time
+  r <- b - 1 
+  # effective reproductive number
+  R <- 1 + r * Tc
+  # Transmission probability
+  p <- R/(C*T) 
+  return(data.frame(R=R, trans_p=p))
+}
