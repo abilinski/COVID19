11414ec8fc7e299e4659855b42882c84509eb617
zhulin0406 <32205290+zhulin0406@users.noreply.github.com>
2020-03-27 16:39:05 -0700

    changed time varing detection calculation from exponential to linear

--- 1 - R Package/R/simple_R_p_calc.R
+++ 1 - R Package/R/simple_R_p_calc.R
@@ -1,11 +1,10 @@
 # fit exponential model with time series data to calculate R and transmission p
-exp_calc <- function(path, Tc=7.5, C=10, T=5) {
-  #path: the folder containing the time_series_SCC.csv file
+exp_calc <- function(Tc=7.5, C=10, T=5) {
   #Tc: mean generation interval, mean duration between time of infection of a secondary infectee and the time of infection of its primary infector
   #C: mean contact (calculated from contact matrix)
   #T: infectiou period
   #data from 2/25/20 to 3/15/20: it was announced on 2/27/20 that people who do not have travel/contact histry can be tested, so not taking data from too early
-  time_series <- read.csv(paste0(path,"time_series_SCC.csv"))
+  time_series <- read.csv(system.file("time_series/time_series_SCC.csv", package="covid.epi"), as.is = T)
   fit <- lm(log(time_series$cum_cases) ~ time_series$time)
   # plot(time_series$time, time_series$cum_cases)
   # lines(time_series$time, exp(fitted(fit)))
