45be6d135fb188864974935b7e75b3fc76afadcc
Christian Testa <testa101@gmail.com>
2020-03-28 13:52:59 -0400

    Adding test for ensuring base case vs. base case run as intv are equal

---
+++ 1 - R Package/tests/testthat/test_interventions.R
@@ -0,0 +1,21 @@
+test_that("base case run as run_basic or run_int works the same", {
+
+  # the motivation of this test is to ensure that an intervention 
+  # with no difference from the base case should have no difference 
+  # from the base case. 
+
+  # this test is important because when interventions are simulated, 
+  # an event is used in the differential equations solver (run_int).
+  # 
+
+  param_vec <- load_parameters()
+
+  test = run_param_vec(
+    params = param_vec, params2=NULL, days_out1 = 30, 
+    days_out2 = NULL, model_type = run_basic)
+
+  test_int = run_param_vec(params = param_vec, params2 = param_vec, 
+    days_out1 = 10, days_out2 = 30, model_type = run_int)
+
+  expect_lt(max(test - test_int), .0001)
+})
