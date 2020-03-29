bccc4f6da77cb64472faf160f3e5116d6615de16
Christian Testa <testa101@gmail.com>
2020-03-29 09:11:41 -0400

    Setting up contact matrix UI integrations
    This commit does a handful of things:
    
    - Adds a UI and server module for the contact matrix user inputs which:
    
      - Renders user inputs for contact matrices for base case vs. interverventions
        (_int) + socially distanced (SD) vs. not socially distanced (NSD) (see
        contact_matrix_shiny_ui_module)
    
      - Automatically updates the contact matrix based on user inputs (see
        contact_matrix_shiny_server_module):
    
        - NSD <- SD * e
    
        - SD_int <- SD
          - we automatically bring changes over from the base case into SD_int so users don't accidentally
            update the base case but not their default assumptions for an intervention. SD_int can still be
            modified after the value in SD_int is updated based on SD
    
        _ NSD_int <- SD_int
          - We assume that "socially distanced is socially distanced"
    
    - Adds significant commenting + documentation to the server.R file
    
    - Reworks the params input to be more explicit about what parameters are used inside server.R

---
+++ 1 - R Package/R/contact_matrix_shiny_ui_module.R
@@ -0,0 +1,83 @@
+
+contact_matrix_ui_for_base_case <- function(param_vec) {
+  fluidRow(
+    column(12,
+      h4("Contact Rates (not-socially-distanced)"),
+      column(4,
+        numericInput("v11", "[0,20) [20,65)", param_vec[['v11']], min=0),
+        numericInput("v12", "[0,20) [20,65)", param_vec[['v12']], min=0),
+        numericInput("v13", "[0,20) 65+", param_vec[['v13']], min=0)
+        ),
+      column(4,
+        numericInput("v21", "[20,65) [0,20)", param_vec[['v21']], min=0),
+        numericInput("v22", "[20,65) [20,65)", param_vec[['v22']], min=0),
+        numericInput("v23", "[20,65) 65+", param_vec[['v23']], min=0)
+        ),
+      column(4,
+        numericInput("v31", "65+ [0,20)", param_vec[['v31']], min=0),
+        numericInput("v32", "65+ [20,65)", param_vec[['v32']], min=0),
+        numericInput("v33", "65+ 65+", param_vec[['v33']], min=0)
+      )
+      ),
+    column(12,
+      h4("Contact Rates (socially-distanced)"),
+      column(4,
+        disabled(numericInput("v11Q", "[0,20) [20,65)", 0, min=0)),
+        disabled(numericInput("v12Q", "[0,20) [20,65)", 0, min=0)),
+        disabled(numericInput("v13Q", "[0,20) 65+", 0, min=0))
+        ),
+      column(4,
+        disabled(numericInput("v21Q", "[20,65) [0,20)", 0, min=0)),
+        disabled(numericInput("v22Q", "[20,65) [20,65)", 0, min=0)),
+        disabled(numericInput("v23Q", "[20,65) 65+", 0, min=0))
+        ),
+      column(4,
+        disabled(numericInput("v31Q", "65+ [0,20)", 0, min=0)),
+        disabled(numericInput("v32Q", "65+ [20,65)", 0, min=0)),
+        disabled(numericInput("v33Q", "65+ 65+", 0, min=0))
+      )
+    )
+  )
+
+}
+
+contact_matrix_ui_for_intervention <- function(param_vec) {
+  fluidRow(
+            column(12,
+              h4("Contact Rates (not-socially-distanced)"),
+              column(4,
+                numericInput("v11_int", "[0,20) [20,65)", param_vec[['v11']], min=0),
+                numericInput("v12_int", "[0,20) [20,65)", param_vec[['v12']], min=0),
+                numericInput("v13_int", "[0,20) 65+", param_vec[['v13']], min=0)
+                ),
+              column(4,
+                numericInput("v21_int", "[20,65) [0,20)", param_vec[['v21']], min=0),
+                numericInput("v22_int", "[20,65) [20,65)", param_vec[['v22']], min=0),
+                numericInput("v23_int", "[20,65) 65+", param_vec[['v23']], min=0)
+                ),
+              column(4,
+                numericInput("v31_int", "65+ [0,20)", param_vec[['v31']], min=0),
+                numericInput("v32_int", "65+ [20,65)", param_vec[['v32']], min=0),
+                numericInput("v33_int", "65+ 65+", param_vec[['v33']], min=0)
+              )
+              ),
+            column(12,
+              h4("Contact Rates (socially-distanced)"),
+              column(4,
+                disabled(numericInput("v11Q_int", "[0,20) [20,65)", 0, min=0)),
+                disabled(numericInput("v12Q_int", "[0,20) [20,65)", 0, min=0)),
+                disabled(numericInput("v13Q_int", "[0,20) 65+", 0, min=0))
+                ),
+              column(4,
+                disabled(numericInput("v21Q_int", "[20,65) [0,20)", 0, min=0)),
+                disabled(numericInput("v22Q_int", "[20,65) [20,65)", 0, min=0)),
+                disabled(numericInput("v23Q_int", "[20,65) 65+", 0, min=0))
+                ),
+              column(4,
+                disabled(numericInput("v31Q_int", "65+ [0,20)", 0, min=0)),
+                disabled(numericInput("v32Q_int", "65+ [20,65)", 0, min=0)),
+                disabled(numericInput("v33Q_int", "65+ 65+", 0, min=0))
+              )
+            )
+    )
+}
