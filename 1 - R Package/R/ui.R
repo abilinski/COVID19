a3a06fffbe614d197c462162aafba81561cbae1b
Christian Testa <testa101@gmail.com>
2020-04-03 16:06:11 -0400

    Disable td, enable p for editing

--- 1 - R Package/R/ui.R
+++ 1 - R Package/R/ui.R
@@ -97,8 +97,8 @@ generate_ui <- function() {
                 numericInput("sim_time", label="simulation time (days)", value=30),
                 # we want to show r0 and td from calculation later
                 disabled(numericInput("R0", label="R0", value=1.0)),
-                disabled(numericInput("p", label="p: Pr(transmission/contact)", value=0.05)),
-                numericInput("td", label="Doubling Time", value=2.5),
+                numericInput("p", label="p: Pr(transmission/contact)", value=0.05),
+                disabled(numericInput("td", label="Doubling Time", value=2.5)),
                 numericInput("delta", label=HTML("&delta;: 1/(dur of incub)"), value=0.2),
                 numericInput("gamma", label=HTML("&gamma;: 1/(dur of infectious)"), value=0.2),
                 numericInput("obs", label="obs cases at day1", value=100),
