b8b623ed419078b8363a27b474828b2cbc2ffc1b
zhulin0406 <32205290+zhulin0406@users.noreply.github.com>
2020-04-03 14:12:36 -0700

    calculate R0 and td from p

---
+++ 1 - R Package/R/calc_R0_td_from_p.R
@@ -0,0 +1,16 @@
+#calculate R0 and td from p
+calc_R0_td_from_p <- function(params, p) {
+  params$p <- p
+  
+  R0 <- R_0(params)
+  
+  f <- function(lamda) (lamda+params$delta)*(lamda+params$gamma)/(params$delta*params$gamma) - R0
+  lamda <- uniroot(f, interval=c(0,70))[[1]]
+  
+  td <- log(2)/lamda
+  
+  return(c(R0, td))
+}
+
+
+
